6736d6f68a93f34f4e51438502a49d0857efa70d
Victor Biasibetti <victorbiasibetti7@gmail.com>
2024-05-08 15:42:57 -0300

    create Default Layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,18 @@
+import Footer from "../Footer";
+import Header from "../Header";
+
+type Props = {
+    children: React.ReactNode
+}
+
+const DefaultLayout = ({children}: Props) => {
+    return (
+        <div>
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    )
+}
+
+export default DefaultLayout;
