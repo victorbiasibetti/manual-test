e1ae863270d9bd50e09ffca5123a39beb04537f8
Victor Biasibetti <victorbiasibetti7@gmail.com>
2024-05-09 16:42:11 -0300

    add header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+import logo from "../../../public/images/logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.wrapper}>
+      <nav className={styles.nav}>
+        <Image src={logo} width={40} height={40} alt="Manual" />
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
