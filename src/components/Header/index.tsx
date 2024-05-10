405c37f19c59a1037f2f10cf0105a57d74239ec4
Victor Biasibetti <victorbiasibetti7@gmail.com>
2024-05-10 12:57:08 -0300

    finish hero section

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,6 +8,17 @@ const Header = () => {
       <nav className={styles.nav}>
         <Image src={logo} width={40} height={40} alt="Manual" />
       </nav>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <h2>Be good</h2>
+          <h2>to yourself</h2>
+          <p>
+            We’re working around the clock to bring you a holistic approach to
+            your wellness. From top to bottom, inside and out.
+          </p>
+          <button> TAKE THE QUIZ </button>
+        </div>
+      </div>
     </div>
   );
 };
