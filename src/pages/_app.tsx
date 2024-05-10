fd964a536b93c6253906324d674afa3a9f094cb0
Victor Biasibetti <victorbiasibetti7@gmail.com>
2024-05-10 12:56:48 -0300

    loading figma font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
 
+const TTNorms = localFont({ src: "../../public/fonts/TTNorms-Regular.woff2" });
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={TTNorms.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
